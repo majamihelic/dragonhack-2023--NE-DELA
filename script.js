f887c5735a880cab5a3f85a3a7d111fb1db239af
majamihelic <maja.mihelic1@outlook.com>
2023-05-13 14:59:22 +0200

    bionic

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+function bionic(str){
+    let beseda = str.split(" ");
+    let bold = [];
+    for (let i = 0; i < beseda.length; i++) {
+        let crke = "";
+        for (let j = 0; j < beseda[i].length / 2; j++) {
+            crke += beseda[i][j];
+        }
+        let ostalo = beseda[i].slice(crke.length);
+        let koncna = "<b>" + crke + "</b>" + ostalo;
+        bold.push(koncna);
+    }
+    return bold.join(" ");
+}
+
+document.body.innerHTML = bionic(document.body.innerHTML);
+
 
